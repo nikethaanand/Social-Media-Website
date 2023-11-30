3de29915134d6143cbefc544c95646296c82d617
Niketha Anand <nikethaanand@gmail.com>
2023-11-29 18:52:13 -0800

    Basic nav setup

--- Twitter/src/main.jsx
+++ Twitter/src/main.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from './SignIn.jsx'
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<SignIn />} />
+       
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ Twitter/src/signIn.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SignIn = () => {
+  return (
+    <>
+      <div className='main-div'>
+        <h1>Twitter</h1>
+
+        
+        <button>Submit</button>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
