2ce649fb0c963982b9352ea46804037fb08bee3b
Niketha Anand <nikethaanand@gmail.com>
2023-12-12 13:20:28 -0800

    Update Profile.jsx

--- Twitter/src/userprofile/Profile.jsx
+++ Twitter/src/userprofile/Profile.jsx
@@ -1,11 +1,13 @@
 
 import React from 'react'
 import Topbar from '../Navbar/topBar';
-import Twitterprofilephoto from './assets/Twitterprofilephoto.png';
+import Twitterprofilephoto from '../assets/Twitterprofilephoto.png';
 
 export default function Profile() {
     return (<>
     
+
+
     <Topbar />
 
     <img src={Twitterprofilephoto} alt="Logo" className="Twitter-logo" />
